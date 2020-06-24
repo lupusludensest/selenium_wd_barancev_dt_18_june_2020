8980460ede12f12406f207af5d221ad304c1432a
LupusLudensEst <gurovvic@gmail.com>
2020-06-23 21:50:28 -0400

    update dt 23 june 2020

---
+++ hw_dt_23_june_2020_1.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome(executable_path = "C:\Webdrivers\chromedriver")
+    request.addfinalizer(wd.quit)
+    return wd
+
+def hw_dt_23_june_2020(driver):
+    driver.get("https://www.google.com/")
+    driver.maximize_window()
+    driver.find_element_by_name("q").send_keys("webdriver")
+    driver.find_element_by_name("btnK").click()
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
---
+++ hw_dt_23_june_2020_2.py
@@ -0,0 +1,31 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# init driver
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.google.com/')
+
+sleep(2)
+
+#Input into search field "webdriver"
+search = driver.find_element(By.NAME, "q")
+search.clear()
+search.send_keys('webdriver')
+sleep(1)
+
+#Click on "Google Search" button
+driver.find_element(By.NAME, 'btnK').click()
+sleep(2)
+
+# Verify if "WebDriver - World Wide Web Consortium" is on page
+assert 'WebDriver - World Wide Web Consortium' in driver.find_element(By.XPATH, "//a[@href='https://www.w3.org/TR/webdriver/']").text
+print('Text: ', driver.find_element(By.XPATH, "//a[@href='https://www.w3.org/TR/webdriver/']").text, ';')
+driver.quit()
+
+
+
+
