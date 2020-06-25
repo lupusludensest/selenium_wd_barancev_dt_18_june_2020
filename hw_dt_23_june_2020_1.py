7466491b273ba26fd4934c355134253fcf8fffc1
LupusLudensEst <gurovvic@gmail.com>
2020-06-25 19:40:02 -0400

    update dt 25 june 2020

--- hw_dt_23_june_2020_1.py
+++ hw_dt_23_june_2020_1.py
@@ -2,17 +2,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome(executable_path = "C:\Webdrivers\chromedriver")
+    wd = webdriver.Chrome()
     request.addfinalizer(wd.quit)
     return wd
 
 def test_example(driver):
     driver.get("https://www.google.com/")
-    driver.maximize_window()
-    driver.find_element_by_name("q").clear()
     driver.find_element_by_name("q").send_keys("webdriver")
+    sleep(2)
     driver.find_element_by_name("btnK").click()
-    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
