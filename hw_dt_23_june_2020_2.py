85d5b7221a06d3b6cf44b6a0d95cc99af1bcb12d
LupusLudensEst <gurovvic@gmail.com>
2020-06-25 20:20:12 -0400

    update dt 25 june 2020

--- hw_dt_23_june_2020_2.py
+++ hw_dt_23_june_2020_2.py
@@ -1,6 +1,8 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # init driver
 driver = webdriver.Chrome()
@@ -23,8 +25,9 @@ sleep(2)
 
 # Verify if "WebDriver - World Wide Web Consortium" is on page
 assert 'WebDriver - World Wide Web Consortium' in driver.find_element(By.XPATH, "//a[@href='https://www.w3.org/TR/webdriver/']").text
-print('Text: ', (driver.find_element(By.XPATH, "//a[@href='https://www.w3.org/TR/webdriver/']").text)[:40], ';')
+print('Text: ', (driver.find_element(By.XPATH, "//a[@href='https://www.w3.org/TR/webdriver/']").text)[:37], ';')
 driver.quit()
+
 
 
 
