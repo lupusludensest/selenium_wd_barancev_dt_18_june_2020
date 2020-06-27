6a7cddf5ee893273837ba3476e77e59d114b1d7d
LupusLudensEst <gurovvic@gmail.com>
2020-06-26 22:30:05 -0400

    update dt 26 june 2020

--- hw_dt_23_june_2020_1.py
+++ hw_dt_23_june_2020_1.py
@@ -6,8 +6,13 @@ from time import sleep
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    # wd = webdriver.Chrome()
+    # wd = webdriver.Chrome(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
     # wd = webdriver.Firefox()
+    options = webdriver.FirefoxOptions()
+    options.binary_location = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
+    options.add_argument("start-maximized")
+    wd = webdriver.Firefox(firefox_options=options)
     # wd = webdriver.Edge(executable_path="C:\Webdrivers\msedgedriver")
     # wd = webdriver.Ie()
     # wd = webdriver.Ie(capabilities={"unexpectedAlertBehaviour": "dismiss"})
@@ -19,6 +24,7 @@ def test_example(driver):
     # pass
     driver.get("https://www.google.com/")
     driver.maximize_window()
+    driver.find_element_by_name("q").clear()
     driver.find_element_by_name("q").send_keys("webdriver")
     sleep(2)
     driver.find_element_by_name("btnK").click()
--- hw_dt_23_june_2020_2.py
+++ hw_dt_23_june_2020_2.py
@@ -1,8 +1,6 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 # init driver
 driver = webdriver.Chrome()
