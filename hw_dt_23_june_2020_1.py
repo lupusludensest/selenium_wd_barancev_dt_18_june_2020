95b1adc5803ea331ea1ebb33370fb76cca6e055b
LupusLudensEst <gurovvic@gmail.com>
2020-06-27 22:12:01 -0400

    update dt 27 june 2020

--- hw_dt_23_june_2020_1.py
+++ hw_dt_23_june_2020_1.py
@@ -7,17 +7,18 @@ from time import sleep
 @pytest.fixture
 def driver(request):
     # wd = webdriver.Chrome()
-    # wd = webdriver.Chrome(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
+    wd = webdriver.Chrome(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
     # wd = webdriver.Firefox()
-    options = webdriver.FirefoxOptions()
-    options.binary_location = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
-    options.add_argument("start-maximized")
-    wd = webdriver.Firefox(firefox_options=options)
+    # options = webdriver.FirefoxOptions()
+    # options.binary_location = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
+    # options.add_argument("start-maximized")
+    # wd = webdriver.Firefox(firefox_options=options)
     # wd = webdriver.Edge(executable_path="C:\Webdrivers\msedgedriver")
     # wd = webdriver.Ie()
     # wd = webdriver.Ie(capabilities={"unexpectedAlertBehaviour": "dismiss"})
-    # print(wd.capabilities)
     # wd = webdriver.Ie(capabilities={"requireWindowFocus": True})
+    # print(f'\nCAPABILITIES: {wd.capabilities}\nEND CAPABILITIES')
+    sleep(4)
     request.addfinalizer(wd.quit)
     return wd
 
@@ -25,10 +26,10 @@ def test_example(driver):
     # pass
     driver.get("https://www.google.com/")
     driver.maximize_window()
-    sleep(4)
+    sleep(8)
     driver.find_element_by_name("q").clear()
-    sleep(4)
+    sleep(8)
     driver.find_element_by_name("q").send_keys("webdriver")
-    sleep(4)
+    sleep(8)
     driver.find_element_by_name("btnK").click()
     WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
