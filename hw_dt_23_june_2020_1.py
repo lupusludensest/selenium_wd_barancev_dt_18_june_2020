5c1caabce698abbbd3a2e5c9fbcb23a400c24a06
Viacheslav Gurov <gurov>
2020-06-28 21:17:55 -0400

    update dt 38 june 2020

--- hw_dt_23_june_2020_1.py
+++ hw_dt_23_june_2020_1.py
@@ -9,17 +9,22 @@ def driver(request):
     # wd = webdriver.Chrome()
     # wd = webdriver.Chrome(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
     # wd = webdriver.Firefox()
-    # options = webdriver.FirefoxOptions()
-    # options.binary_location = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
-    # options.add_argument("start-maximized")
-    # wd = webdriver.Firefox(firefox_options=options)
+    options = webdriver.FirefoxOptions()
+    options.binary_location = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
+    options.add_argument("start-maximized")
+    wd = webdriver.Firefox(firefox_options=options)
+    # new method
+    # wd = webdriver.Firefox()
+    # new method more obviously
+    # wd = webdriver.Firefox(capabilities={"marionette": True})
+    # old method
+    # wd = webdriver.Firefox(capabilities={"marionette": False})
     # wd = webdriver.Edge(executable_path="C:\Webdrivers\msedgedriver")
     # wd = webdriver.Ie()
     # wd = webdriver.Ie(capabilities={"unexpectedAlertBehaviour": "dismiss"})
     # wd = webdriver.Ie(capabilities={"requireWindowFocus": True})
-    wd = webdriver.Ie(capabilities={"IntroduceInstabilityByIgnoringProtectedModeSettings": True, "requireWindowFocus": True})
-    print(f'\nCAPABILITIES: {wd.capabilities}\nEND CAPABILITIES')
-    sleep(4)
+    # wd = webdriver.Ie(capabilities={"IntroduceInstabilityByIgnoringProtectedModeSettings": True, "requireWindowFocus": True, "unexpectedAlertBehaviour": "dismiss", "ignoreZoomSetting": True})
+    # print(f'\nCAPABILITIES: {wd.capabilities}\nEND CAPABILITIES')
     request.addfinalizer(wd.quit)
     return wd
 
@@ -27,10 +32,10 @@ def test_example(driver):
     # pass
     driver.get("https://www.google.com/")
     driver.maximize_window()
-    sleep(8)
+    sleep(2)
     driver.find_element_by_name("q").clear()
-    sleep(8)
+    sleep(2)
     driver.find_element_by_name("q").send_keys("webdriver")
-    sleep(8)
+    sleep(2)
     driver.find_element_by_name("btnK").click()
     WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
