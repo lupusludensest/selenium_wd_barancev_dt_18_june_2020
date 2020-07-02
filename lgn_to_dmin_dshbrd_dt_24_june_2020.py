eaeefad1abe1662f2685c1363d4b6093f9fa6950
Viacheslav Gurov <gurov>
2020-07-01 20:16:11 -0400

    update dt 01 july 2020

--- lgn_to_dmin_dshbrd_dt_24_june_2020.py
+++ lgn_to_dmin_dshbrd_dt_24_june_2020.py
@@ -34,18 +34,22 @@ print('Text: ', (driver.find_element(By.ID, "content").text)[:8], ';')
 #1 Click on Appearance
 driver.find_element(By.CSS_SELECTOR, 'span.name').click()
 # для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
 
 #1.1 Click on Template
 driver.find_element(By.XPATH, "//span[contains(text(), 'Template')]").click()
 # для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
 
 #1.2 Click on Logotype
 driver.find_element(By.XPATH, "//span[contains(text(), 'Logotype')]").click()
 # для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
 
 #2 Click on Catalog
 driver.find_element(By.XPATH, "//span[@class='fa-stack fa-lg icon-wrapper'][1]").click()
 # для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
 
 #2.1 Click on Catalog
 driver.find_element(By.XPATH, "//span[contains(text(), 'Catalog')]").click()
@@ -95,6 +99,7 @@ sleep(2)
 #3 Click on Countries
 driver.find_element(By.XPATH, "//span[contains(text(), 'Countries')]").click()
 # для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
 
 #4 Click on Currencies
 driver.find_element(By.XPATH, "//span[contains(text(), 'Currencies')]").click()
@@ -104,10 +109,88 @@ sleep(2)
 #5 Click on Customers
 driver.find_element(By.XPATH, "//i[@class='fa fa-user fa-stack-1x icon'][1]").click()
 # для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
 
 #5.1 Click on Customers
 driver.find_element(By.XPATH, "//span[contains(text(), 'Customers')]").click()
 # для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#5.2 Click on CSV Import/Export
+driver.find_element(By.XPATH, "//span[contains(text(), 'CSV Import/Export')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#5.3 Click on CSV Newsletter
+driver.find_element(By.XPATH, "//span[contains(text(), 'Newsletter')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#6 Click on Geo Zones
+driver.find_element(By.XPATH, "//span[contains(text(), 'Geo Zones')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#7 Click on Languages
+driver.find_element(By.XPATH, "//i[@class='fa fa-comments fa-stack-1x icon'][1]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#7.1 Click on Languages
+driver.find_element(By.ID, "doc-languages").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#7.2 Click on Storage Encoding
+driver.find_element(By.XPATH, "//span[contains(text(), 'Storage Encoding')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#8 Click on Modules
+driver.find_element(By.XPATH, "//i[@class='fa fa-cube fa-stack-1x icon'][1]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#8.1 Click on Background Jobs
+driver.find_element(By.XPATH, "//span[contains(text(), 'Background Jobs')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#8.2 Click on Customer
+driver.find_element(By.ID, "doc-customer").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(3)
+
+#8.3 Click on Shipping
+driver.find_element(By.LINK_TEXT, "Shipping").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#8.4 Click on Payment
+driver.find_element(By.XPATH, "//span[contains(text(), 'Payment')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#8.5 Click on Order Total
+driver.find_element(By.XPATH, "//span[contains(text(), 'Order Total')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#8.6 Click on Order Success
+driver.find_element(By.XPATH, "//span[contains(text(), 'Order Success')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#8.7 Click on Order Action
+driver.find_element(By.XPATH, "//span[contains(text(), 'Order Action')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
+#9 Click on Orders
+driver.find_element(By.XPATH, "//span[contains(text(), 'Orders')]").click()
+# для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+sleep(2)
+
 
 
 
