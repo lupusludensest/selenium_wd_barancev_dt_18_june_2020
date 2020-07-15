4034dc45e1470d85308e8ed86ce14cc49003eff0
Viacheslav Gurov <gurov>
2020-07-14 22:26:12 -0400

    update dt 14 july 2020

--- homework12_dt_13_july_2020_0.py
+++ homework12_dt_13_july_2020_0.py
@@ -7,6 +7,7 @@ from time import sleep
 from selenium.webdriver.support.ui import Select
 from random import randint
 import os
+import datetime
 
 @pytest.fixture
 def driver(request):
@@ -29,7 +30,7 @@ def driver(request):
     # wd = webdriver.Ie(capabilities={"requireWindowFocus": True})
     # wd = webdriver.Ie(capabilities={"IntroduceInstabilityByIgnoringProtectedModeSettings": True, "requireWindowFocus": True, "unexpectedAlertBehaviour": "dismiss", "ignoreZoomSetting": True})
     # print(f'\nCAPABILITIES: {wd.capabilities}\nEND CAPABILITIES')
-    print(f'WD capabilities: {wd.capabilities}\n')
+    print(f'\nWD capabilities: {wd.capabilities}\n')
     request.addfinalizer(wd.quit)
     return wd
 
@@ -42,16 +43,13 @@ def test_litecart(driver):
     # Enter login: admin
     driver.find_element(By.NAME, "username").clear()
     driver.find_element(By.NAME, "username").send_keys("admin")
-    sleep(2)
 
     # Enter password: admin
     driver.find_element(By.NAME,"password").clear()
     driver.find_element(By.NAME,"password").send_keys("admin")
-    sleep(2)
 
     # Click Login button
     driver.find_element(By.NAME,"login").click()
-    sleep(2)
 
     # Click Catalog button
     driver.find_element(By.XPATH, "//span[contains(text(), 'Catalog')]").click()
@@ -66,54 +64,122 @@ def test_litecart(driver):
     driver.find_element(By.XPATH, "//a[contains(text(), 'General')]").click()
     sleep(2)
 
+    # Click on Enabled radio button
+    if not driver.find_element(By.NAME, "status").is_selected():
+        driver.find_element(By.NAME, "status").click()
+    sleep(2)
+
     # Fill out Name field
     driver.find_element(By.NAME, "name[en]").clear()
     driver.find_element(By.NAME,"name[en]").send_keys("Abirvalg")
-    sleep(2)
 
     # Fill out Code field
     driver.find_element(By.NAME, "code").clear()
     driver.find_element(By.NAME,"code").send_keys("428958")
-    sleep(2)
 
     # Check on Unisex checkbox
     driver.find_element(By.XPATH, "//input[@value='1-3']").click()
-    sleep(2)
 
     # Fill out Quantity field
     driver.find_element(By.XPATH, "//input[@name='quantity']").clear()
-    driver.find_element(By.XPATH,"//input[@name='quantity']").send_keys("4")
-    sleep(2)
+    driver.find_element(By.XPATH,"//input[@name='quantity']").send_keys("1")
 
     # Fill out Quantity Unit field
-    driver.find_element(By.NAME, "quantity_unit_id").click()
-    sleep(2)
+    quantity = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "quantity_unit_id")))
+    quantity.click()
+    selector = Select(quantity)
+    selector.select_by_visible_text('pcs')
+    print(f'\nQuantity: {quantity}; Selector: {selector}\n')
 
     # Fill out Delivery Status field
-    driver.find_element(By.NAME, "delivery_status_id").click()
-    sleep(2)
+    delivery = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "delivery_status_id")))
+    delivery.click()
+    selector = Select(delivery)
+    selector.select_by_visible_text('3-5 days')
+    print(f'\nDelivery Status: {delivery}; Selector: {selector}\n')
 
     # Fill out Sold Out Status field
-    driver.find_element(By.NAME, "sold_out_status_id").click()
-    sleep(2)
+    sold_out = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "sold_out_status_id")))
+    sold_out.click()
+    selector = Select(sold_out)
+    selector.select_by_visible_text('Temporary sold out')
+    print(f'\nSold Out Status: {sold_out}; Selector: {selector}\n')
 
     # Upload a pic by clicking on Choose File button
-    product_image = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),'FB_IMG_1575747160887.png'))
-    print(product_image)
-    sleep(2)
+    product_image = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),'Guessed_baby_face.png'))
+    driver.find_element(By.NAME, "new_images[]").send_keys(product_image)
+    print(f'\nProduct image: {product_image}')
 
-    # Set data Date Valid From
-    # driver.find_element(By.NAME, "date_valid_from").click()
-    # sleep(2)
-    element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "date_valid_from")))
-    element.click()
-    sleep(2)
-    selector = Select(element)
-    selector.select_by_visible_text('Today')
-    print(f'\nElement: {element}; Selector: {selector}\n')
+    # # Set data Date Valid From
+    date_from = datetime.date.today()
+    driver.find_element(By.NAME, "date_valid_from").send_keys(date_from.strftime('%m-%d-%Y'))
+    date_to = datetime.date.today() + datetime.timedelta(days=15)
+    driver.find_element(By.NAME, "date_valid_to").send_keys(date_to.strftime('%m-%d-%Y'))
 
+    # Click on Information folder
+    driver.find_element(By.CSS_SELECTOR, "[href='#tab-information']").click()
 
+    # Select Manufacturer from drop down menu
+    manufacturer = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "manufacturer_id")))
+    manufacturer.click()
+    selector = Select(manufacturer)
+    selector.select_by_visible_text('ACME Corp.')
+    print(f'\nManufacturer: {manufacturer}; Selector: {selector}\n')
 
+    # Fill out Keywords field
+    driver.find_element(By.NAME, "keywords").clear()
+    driver.find_element(By.NAME,"keywords").send_keys("Abirvalg")
 
+    # Fill out Short Description field
+    driver.find_element(By.NAME, "short_description[en]").clear()
+    driver.find_element(By.NAME,"short_description[en]").send_keys("Abirvalg-is what Sharikov said in Dog's heart")
 
+    # Fill out Description field
+    driver.find_element(By.CSS_SELECTOR, "div.trumbowyg-editor").clear()
+    driver.find_element(By.CSS_SELECTOR, "div.trumbowyg-editor").send_keys("Abirvalg-is what Sharikov said in Dog's heart by M.A. Bulgakov")
 
+    # Fill out Head Title field
+    driver.find_element(By.NAME, "head_title[en]").clear()
+    driver.find_element(By.NAME,"head_title[en]").send_keys("Avirvalg != Gravriba")
+
+    # Fill out Meta Description field
+    driver.find_element(By.NAME, "meta_description[en]").clear()
+    driver.find_element(By.NAME,"meta_description[en]").send_keys("Avirvalg != Gravriba. Metadescription")
+
+    # Click on Prices folder
+    driver.find_element(By.CSS_SELECTOR, "[href='#tab-prices']").click()
+
+    # Fill out Purchase Price field
+    driver.find_element(By.NAME, "purchase_price").clear()
+    driver.find_element(By.NAME, "purchase_price").send_keys("999999")
+
+    # Select Currency from drop down menu
+    currency = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "purchase_price_currency_code")))
+    currency.click()
+    selector = Select(currency)
+    selector.select_by_visible_text('US Dollars')
+    print(f'\nCurrency: {currency}; Selector: {selector}\n')
+
+    # Select Tax Class from drop down menu
+    tax_class = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "tax_class_id")))
+    tax_class.click()
+    selector = Select(tax_class)
+    selector.select_by_visible_text('Standard')
+    print(f'\nTax Class: {tax_class}; Selector: {selector}\n')
+
+    # Fill out Price Incl. Tax (?) USD field
+    driver.find_element(By.NAME, "gross_prices[USD]").clear()
+    driver.find_element(By.NAME, "gross_prices[USD]").send_keys("999")
+
+    # Fill out Price Incl. Tax (?) EURO field
+    driver.find_element(By.NAME, "gross_prices[EUR]").clear()
+    driver.find_element(By.NAME, "gross_prices[EUR]").send_keys("9999")
+
+    # Click on Save button save
+    driver.find_element(By.NAME, "save").click()
+
+    # Verify Abirvalg as a new good is here in Catalog
+    xpath_avirvalg = './/a[contains(.,\'' + "Abirvalg" + '\')]'
+    print(xpath_avirvalg)
+    catalog = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "form[name=catalog_form")))[0]
+    catalog.find_element(By.XPATH, xpath_avirvalg).click()
