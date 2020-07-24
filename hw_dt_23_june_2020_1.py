3f30cc760046cdc30f40a42784cc059afd1c7c73
Viacheslav Gurov <gurov>
2020-07-23 22:29:41 -0400

    dt 23 july 2020

--- hw_dt_23_june_2020_1.py
+++ hw_dt_23_june_2020_1.py
@@ -35,16 +35,28 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+
+class MyListener(AbstractEventListener):
+    def before_find(self, by, value, driver):
+        print(by, value)
+    def after_find(self, by, value, driver):
+        print(by, value, "Element found")
+    def on_exception(self, exception, driver):
+        print(exception)
 
 @pytest.fixture
 def driver(request):
     # wd = webdriver.Chrome()
+    # wd = EventFiringWebDriver(webdriver.Chrome(), MyListener())
     # wd = webdriver.Chrome(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
     # wd = webdriver.Firefox()
-    options = webdriver.FirefoxOptions()
-    options.binary_location = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
-    options.add_argument("start-maximized")
-    wd = webdriver.Firefox(firefox_options=options)
+    # wd = EventFiringWebDriver(webdriver.Firefox(), MyListener())
+    # options = webdriver.FirefoxOptions()
+    # options.binary_location = "C:\\Program Files\\Firefox Nightly\\firefox.exe"
+    # options.add_argument("start-maximized")
+    # wd = webdriver.Firefox(firefox_options=options)
     # new method
     # wd = webdriver.Firefox()
     # new method more obviously
@@ -52,6 +64,7 @@ def driver(request):
     # old method
     # wd = webdriver.Firefox(capabilities={"marionette": False})
     # wd = webdriver.Edge(executable_path="C:\Webdrivers\msedgedriver")
+    wd = EventFiringWebDriver(webdriver.Edge(executable_path="C:\Webdrivers\msedgedriver"), MyListener())
     # wd = webdriver.Ie()
     # wd = webdriver.Ie(capabilities={"unexpectedAlertBehaviour": "dismiss"})
     # wd = webdriver.Ie(capabilities={"requireWindowFocus": True})
@@ -66,9 +79,9 @@ def test_example(driver):
     driver.get("https://www.google.com/")
     driver.maximize_window()
     sleep(2)
-    driver.find_element_by_name("q").clear()
+    driver.find_element(By.NAME, "q").clear()
     sleep(2)
-    driver.find_element_by_name("q").send_keys("webdriver")
+    driver.find_element(By.NAME, "q").send_keys("webdriver")
     sleep(2)
-    driver.find_element_by_name("btnK").click()
+    driver.find_element(By.NAME, "btnK").click()
     WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
--- hw_dt_23_june_2020_2.py
+++ hw_dt_23_june_2020_2.py
@@ -1,3 +1,35 @@
+# [+] Задание 4. Научитесь запускать разные браузеры
+# Попробуйте запустить разработанный ранее сценарий логина во всех основных браузерах, доступных для вашей операционной системы.
+#
+# Windows -- запустите в Firefox (с использованием geckodriver), Chrome, Internet Explorer (или Edge).
+#
+# Linux -- запустите в Firefox (с использованием geckodriver) и Chrome.
+#
+# MacOS -- запустите в Firefox (с использованием geckodriver), Chrome, а при наличии Safari 10 также и в нём.
+#
+# Если всё получилось -- можете самостоятельно отметить задание как выполненное -- перейти на главную страницу курса и поставить галочку напротив этого задания.
+#
+# Если возникли затруднения -- задавайте вопросы в скайп-чат.
+#
+# Submission status
+# Attempt number	This is attempt 1.
+# Submission status	This assignment does not require you to submit anything online
+# Grading status	Not graded
+# Last modified	-
+# __________________________________________________________________
+# [+] Задание 6. Научитесь запускать Firefox Nightly
+# Попробуйте запустить разработанный ранее сценарий логина в браузере Firefox Nightly (https://nightly.mozilla.org/). Если Selenium не может сам найти место, куда установлен Firefox Nightly -- укажите в параметрах запуска путь к браузеру.
+#
+# Если всё получилось -- можете самостоятельно отметить задание как выполненное -- перейти на главную страницу курса и поставить галочку напротив этого задания.
+#
+# Если возникли затруднения -- задавайте вопросы в скайп-чат.
+#
+# Submission status
+# Attempt number	This is attempt 1.
+# Submission status	This assignment does not require you to submit anything online
+# Grading status	Not graded
+# Last modified	-
+
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
